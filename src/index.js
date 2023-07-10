ee4f64e4a01569465c83990b41cce524d1d4e571
siddht1 <rt1yrru+github@gmail.com>
2023-07-10 13:00:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
